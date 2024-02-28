a9b35d996d9d9638eb3fcc188081c20c28ff4acb
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-02-28 20:10:38 +0000

    add readline library & question

--- nodepop/init-db.js
+++ nodepop/init-db.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// import library to add a security question
+const readline = require('node:readline');
 //connect to db
 const connection = require('./lib/connectMongoose');
 //import Advert
@@ -10,6 +12,15 @@ const exadverts = require('./ex-adverts.json')
 
 async function main() {
 
+    //to wait until we are connected to db
+    await new Promise((resolve) => connection.once('open', resolve) )
+
+    //ask question to add security on delete all db
+    const deletedb = await question(`WARNING! You are going to delete all content\nThis action cannot be undone\nAre you sure you want to delete all database content?\nPress ENTER to cancel or YES to confirm: `)
+   
+    if (!deletedb) {
+        process.exit();
+    }
     await initAdverts();
 
     connection.close();
@@ -27,6 +38,21 @@ async function initAdverts() {
     console.log(`Created ${inserted.length} adverts.`)
 
 }
+
+function question(text) {
+    return new Promise((resolve, reject) => {
+        //connect readline
+        const rl = readline.createInterface({
+            input: process.stdin, 
+            output: process.stdout,
+        });
+        rl.question(text, resp => {
+            rl.close();
+            resolve(resp.toLowerCase() === 'yes');
+        })
+    });
+}
+
 
 // call function main and handle error in case promise return error
 main().catch(err => console.log('Error:', err));
