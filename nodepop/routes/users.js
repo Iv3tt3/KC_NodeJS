a5837fe974acbc4966c57d1f7fa0e1f5979cbec0
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-02-29 11:50:05 +0000

    ENV variable to control render error in production

--- nodepop/routes/users.js
+++ nodepop/routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  try{
   res.send('respond with a resource');
+  } catch (error) {
+    next(error)
+  }
 });
 
 module.exports = router;
